07fc097cc0ea756d7b66fd73004fe46c3db12b78
Movitz Sunar <movitz.sunar@gmail.com>
2024-05-08 15:59:44 +0200

    added event + removed hardware etc

--- 2024-final/sanity.py
+++
@@ -1,8 +0,0 @@
-from challtools.utils import discover_challenges, load_config
-
-paths = discover_challenges()
-
-configs = [load_config(path.parent, search=False, cd=False) for path in paths]
-
-print("challs:", len(set(paths)))
-print("ids:", len(set([config["challenge_id"] for config in configs])))
--- 2024-kval/intro/elf101/main.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    puts("Good job! Here is the flag: SSM{elf_on_the_shelf}");
-}
