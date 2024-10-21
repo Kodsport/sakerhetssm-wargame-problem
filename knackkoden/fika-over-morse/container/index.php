13f9b1e31ce588e3a86e8f16f6ed2e2e7702a57a
nnewram <marwenn02@gmail.com>
2024-10-21 07:05:49 +0200

    add fika over morse

---
+++ knackkoden/fika-over-morse/container/index.php
@@ -0,0 +1,71 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Sandlådan</title>
+    <style>
+            body { font-family: Courier; background-color: black; } 
+            h1 { color: #00FF00; }
+            button { color: #0000FF; }
+            h2 {
+                width: 73ch;
+                color: #00FF00;
+                animation: typing 4s steps(37), blink .5s step-end infinite alternate;
+                white-space: nowrap;
+                overflow: hidden;
+                border-right: 3px solid;
+                font-size: 2em;
+            }
+            img {
+                max-height: 20px;
+            }
+            @keyframes typing {
+                from {
+                    width: 0
+                }
+            }
+                
+            @keyframes blink {
+                50% {
+                    border-color: transparent
+                }
+            }
+    </style>
+    <script src="/script.js"></script>
+</head>
+<body id="b">
+    <h2>
+        SSM{
+        <img src="https://giffla.re/gifflar.png" style="transform: scaleX(2); margin-left: 8px; margin-right: 8px;">
+        <img src="https://giffla.re/gifflar.png" style="transform: scaleX(2); margin-left: 8px; margin-right: 8px;">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png" style="opacity:0; max-width: 10px;">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png" style="opacity:0; max-width: 10px;">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png" style="transform: scaleX(2); margin-left: 8px; margin-right: 8px;">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png" style="opacity:0; max-width: 10px;">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png" style="transform: scaleX(2); margin-left: 8px; margin-right: 8px;">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png" style="opacity:0; max-width: 10px;">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png" style="transform: scaleX(2); margin-left: 8px; margin-right: 8px;">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png" style="opacity:0; max-width: 10px;">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png" style="transform: scaleX(2); margin-left: 8px; margin-right: 8px;">
+        <img src="https://giffla.re/gifflar.png" style="opacity:0; max-width: 10px;">
+        <img src="https://giffla.re/gifflar.png">
+        <img src="https://giffla.re/gifflar.png" style="transform: scaleX(2); margin-left: 8px; margin-right: 8px;">
+        <img src="https://giffla.re/gifflar.png">
+        }
+    </h2>
+</body>
+
+</html>
