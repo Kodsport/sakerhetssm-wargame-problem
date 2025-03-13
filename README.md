fb6feebd3c69470964fac692c9b8b99666dfb95f
Liam Thorell <liamthorell18@gmail.com>
2025-03-13 17:27:02 +0100

    Fixed ports in challenges and updated README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # wargame-problem
+To add new challenges to the wargame, the challenge.yml of each chall need to be modified. To find vacant ports, run `python3 ./utils/find_vacant_port.py`, port range should be 40000-50000.
+```yml
+service:
+  type: website
+  image: container
+  internal_port: 1337
+  external_port: 44444 # Make sure to add external_port (use ./utils/find_vacant_port.py to find a vacant port)
+
+# website service
+predefined_services:
+- type: website
+  url: http://188.126.67.132:44444 # Change this port to the external_port
+
+# tcp service
+predefined_services:
+  - type: tcp
+    host: 188.126.67.132
+    port: 44444 # Change this port to the external_port
+
+human_metadata:
+  event_name: SSM 2024 Kval # This should be the event-name as its displayed on https://sakerhetssm.se/challenges
+
+score: 450 # Don't forget to add score
+
+custom:
+  publish: true # If the challenge should be published on https://sakerhetssm.se/challenges
+```
--- utils/find_vacant_port.py
+++ utils/find_vacant_port.py
@@ -7,35 +7,69 @@ END_PORT = 50000
 LIMIT = 5
 VERBOSE = False
 
+# You do not need to run this with args :)
 if len(sys.argv) > 1:
-    START_PORT = int(sys.argv[1])  # Arg1: Start port
+    START_PORT = int(sys.argv[1])
 if len(sys.argv) > 2:
-    END_PORT = int(sys.argv[2])  # Arg2: End port
+    END_PORT = int(sys.argv[2])
 if len(sys.argv) > 3:
-    LIMIT = int(sys.argv[3])  # Arg3: Limit
+    LIMIT = int(sys.argv[3])
 if len(sys.argv) > 4:
-    VERBOSE = bool(sys.argv[4])  # Arg4: Verbose
+    VERBOSE = bool(sys.argv[4])
 
 start = "."
 max_back = 10
 
-while "ctf.yml" not in os.listdir(start):
+# Find the root of the repository
+while "README.md" not in os.listdir(start):
     start += "/.."
     max_back -= 1
     if max_back == 0:
-        print("ctf.yml not found")
+        print(
+            "README.md not found. Make sure README.md is in the root of this repository"
+        )
         exit(1)
 
 
 taken_ports = []
+used_challenge_ids = []
 
+
+def check_port(port, name):
+    if port < START_PORT or port >= END_PORT:
+        print(f"Port {port} is outside of the range {START_PORT}-{END_PORT} in {name}")
+
+
+def add_challenge_id(challenge_id, name):
+    if challenge_id in used_challenge_ids:
+        print(f"Challenge ID {challenge_id} is already in use in {name}")
+    used_challenge_ids.append(challenge_id)
+
+
+# Find all currently taken ports
 for r in os.walk(start):
     for name in ["challenge.yml", "challenge.yaml"]:
         if name in r[2]:
             with open(r[0] + f"/{name}", "rt", encoding="utf8") as f:
                 data = yaml.load(f, Loader=yaml.FullLoader)
+                if "challenge_id" in data:
+                    add_challenge_id(data["challenge_id"], r[0])
                 if "service" in data and "external_port" in data["service"]:
                     taken_ports.append(data["service"]["external_port"])
+                    check_port(data["service"]["external_port"], r[0])
+                if "deployment" in data and "containers" in data["deployment"]:
+                    if "containers" in data["deployment"]:
+                        for container in data["deployment"]["containers"].values():
+                            if "services" in container:
+                                for service in container["services"]:
+                                    if "external_port" in service:
+                                        taken_ports.append(service["external_port"])
+                                        check_port(service["external_port"], r[0])
+                            if "extra_exposed_ports" in container:
+                                for ports in container["extra_exposed_ports"]:
+                                    if "external_port" in ports:
+                                        taken_ports.append(ports["external_port"])
+                                        check_port(ports["external_port"], r[0])
 
 taken_ports.sort()
 printed = 0
